666fa16574fe32fabf1d8b8dcd5f42645ea04626
stopitgetsomehelp <ihatenetmail@gmail.com>
2020-01-12 21:25:57 +0700

    Create camera and change GameObject from Rectangle to Polygon

--- core/src/com/education/coinman/game/view/GameScreen.java
+++ core/src/com/education/coinman/game/view/GameScreen.java
@@ -3,6 +3,7 @@ package com.education.coinman.game.view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.education.coinman.game.model.CoinMan;
@@ -13,6 +14,7 @@ public class GameScreen implements Screen {
     private Texture coinManTexture;
     private SpriteBatch batch;
     private CoinMan coinMan;
+    private OrthographicCamera camera;
 
     @Override
     public void show() {
@@ -20,14 +22,17 @@ public class GameScreen implements Screen {
         bgTexture = new Texture("bg.png");
         coinManTexture = new Texture("frame-1.png");
         coinMan = new CoinMan(coinManTexture,0,0,coinManTexture.getWidth() / 2, coinManTexture.getHeight() / 2);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
-        //is commit system working?
+
+        batch.setProjectionMatrix(camera.combined);
         batch.draw(bgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         coinMan.draw(batch);
 
@@ -36,7 +41,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        float aspectRatio = (float) width / height;
+        camera.translate(camera.viewportWidth / 4,camera.viewportHeight / 4);
+        camera.update();
     }
 
     @Override
