07dfc1e3c696fec3b4cf83a20dd6edc7add42281
stopitgetsomehelp <ihatenetmail@gmail.com>
2020-01-12 19:45:00 +0700

    test commit

--- core/src/com/education/coinman/game/view/GameScreen.java
+++ core/src/com/education/coinman/game/view/GameScreen.java
@@ -27,8 +27,10 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+
         batch.draw(bgTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         coinMan.draw(batch);
+
         batch.end();
     }
 
